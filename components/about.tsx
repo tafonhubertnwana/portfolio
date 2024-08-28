29016881fb2fa4c10b51ed2b619b41d19370f4d0
Tafon_Hubert <tafonsoftwarespecialist@gmail.com>
2024-08-28 01:06:27 +0100

    making changes

--- components/about.tsx
+++ components/about.tsx
@@ -8,7 +8,7 @@ const About = () => {
     <> 
     <div className='dark:bg-slate-800'>
       
-        <div id='about' className="container mx-auto h-100 px-4  pt-[6%] ">
+        <div id='about' className="container mx-auto h-100 px-4 pt-[20%] md:pt-[6%] ">
             <div className='flex flex-col items-center'>
               <h4 className="flex justify-center items-center text-[#31473a] ">About Me</h4>
               <div className="mt-1 w-24">
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -97,7 +97,7 @@ const Navbar = () => {
             className={`${
               nav
                 ? 'md:hidden fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-white px-8 py-1 ease-in duration-500 dark:bg-slate-800'
-                : 'md:hidden fixed left-[-100%] top-0 py-5 ease-in duration-500 dark:bg-slate-800'
+                : 'md:hidden fixed left-[-100%] top-0 py-5 ease-in duration-200 dark:bg-slate-800'
             }`}
           >
             <div className="flex w-full justify-between border-b">
